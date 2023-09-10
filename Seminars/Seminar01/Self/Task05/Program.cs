0301feba898337ab75a02307bc608f7180d22eab
Zdestr <127941980+Zdestr@users.noreply.github.com>
2023-09-10 21:30:37 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,22 @@
-﻿// Your code here!
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите напряжение системы и сопротивление:\n");
+            double U = double.Parse(Console.ReadLine()),
+                   R = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сила тока равна {0} А\nМощность тока равна {1} Вт", U / R, U * U / R);
+            Console.ReadKey();
+
+        }
+    }
+}
