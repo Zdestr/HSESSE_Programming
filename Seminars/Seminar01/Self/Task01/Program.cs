f35f6dc3ae75fc44a46678d26dcdda9b563fe8b4
Zdestr <127941980+Zdestr@users.noreply.github.com>
2023-09-10 21:10:24 +0300

    Program.cs
    Task 1

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,21 @@
-﻿// Your code here!
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string Surname = Console.ReadLine(),
+                   Name    = Console.ReadLine(),
+                   FName   = Console.ReadLine();
+
+            Console.WriteLine("\nФамилия: " + Surname + "\nИмя: " + Name + "\nОтчество: " + FName);
+            Console.ReadKey();
+        }
+    }
+}
