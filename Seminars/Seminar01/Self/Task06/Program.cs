03990261f1e1de7e8bca4c03e666e7c59fefb02f
Zdestr <127941980+Zdestr@users.noreply.github.com>
2023-09-10 21:33:02 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины 2 катетов:\n");
+            double A = double.Parse(Console.ReadLine()),
+                   B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Гипотенуза равна {0}", Math.Sqrt(A*A + B*B));
+            Console.ReadKey();
+
+        }
+    }
+}
