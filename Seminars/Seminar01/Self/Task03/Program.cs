80cf7d67032349295113e558af23bb8947f13a80
Zdestr <127941980+Zdestr@users.noreply.github.com>
2023-09-10 21:15:38 +0300

    Program.cs

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,19 @@
-﻿// Your code here!
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("\nЗдравствуйте, " + Name + "!");
+            Console.ReadKey();
+        }
+    }
+}
