83eb497cceffdfa66da822b85f6e5cf39b7c9a84
Zdestr <127941980+Zdestr@users.noreply.github.com>
2023-09-10 21:21:41 +0300

    Program.cs

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,21 @@
-﻿// Your code here!
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = (ConsoleColor) 3;
+            Console.ForegroundColor = (ConsoleColor) 2;
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("\nЗдравствуйте, {0}!", Name);
+            Console.ReadKey();
+        }
+    }
+}
